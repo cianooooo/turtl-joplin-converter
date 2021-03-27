68ef6fcc1e8809228b95e28109a0bcd0dc480b15
cianooo <28299281+cianooooo@users.noreply.github.com>
2021-03-27 11:35:22 +0000

    Image attachments working

--- turtl_joplin_converter.rb
+++ turtl_joplin_converter.rb
@@ -1,7 +1,13 @@
+unless ARGV.length == 1
+  puts 'Pass the file as an argument'
+  exit
+end
+
 require 'json'
 require 'fileutils'
 require 'securerandom'
 require 'time'
+require 'base64'
 require 'byebug'
 
 class TurtlJoplinConverter
@@ -24,7 +30,6 @@ class TurtlJoplinConverter
   end
 
   def extract_spaces
-    puts 'Extracting Spaces from Turtl data...'
     @spaces = @data['spaces'].each_with_object([]) do |space, array|
       array << {
         id: space['id'],
@@ -34,7 +39,6 @@ class TurtlJoplinConverter
   end
 
   def extract_boards
-    puts 'Extracting Boards from Turtl data...'
     @boards = @data['boards'].each_with_object([]) do |board, array|
       array << {
         id: board['id'],
@@ -45,7 +49,6 @@ class TurtlJoplinConverter
   end
 
   def extract_files_data
-    puts 'Extracting Files from Turtl data...'
     @files_data = @data['files'].each_with_object([]) do |file, array|
       array << {
         id: file['id'],
@@ -55,12 +58,11 @@ class TurtlJoplinConverter
   end
 
   def extract_notes
-    puts 'Extracting Notes from Turtl data...'
     @notes = @data['notes'].each_with_object([]) do |note, array|
       array << {
         id: note['id'],
         board_id: note['board_id'],
-        file: note['has_file'] ? merge_file(note['file'], note['id']) : nil,
+        file: fetch_file(note),
         space_id: note['space_id'],
         tags: note['tags'],
         markdown: convert_markdown(note['text']),
@@ -70,12 +72,18 @@ class TurtlJoplinConverter
     end
   end
 
+  def fetch_file(note)
+    return unless note['has_file'] || note.dig('file', 'name')
+
+    merge_file(note['file'], note['id'])
+  end
+
   def merge_file(file, id)
     file.merge(data: @files_data.select { |file_data| file_data[:id] == id })
   end
 
   def convert_markdown(markdown)
-    markdown.gsub('__', '**').gsub('---', '* * *')
+    markdown.gsub('__', '**').gsub(/-{3,}/, '* * *')
   end
 
   def convert_data
@@ -83,7 +91,6 @@ class TurtlJoplinConverter
   end
 
   def convert_spaces
-    puts 'Converting Spaces to Joplin data...'
     @joplin_spaces = @spaces.map do |space|
       {
         id: generate_id,
@@ -96,7 +103,6 @@ class TurtlJoplinConverter
   end
 
   def convert_boards
-    puts 'Converting Boards to Joplin data...'
     @joplin_boards = @boards.map do |board|
       {
         id: generate_id,
@@ -109,13 +115,13 @@ class TurtlJoplinConverter
   end
 
   def convert_notes
-    puts 'Converting Notes to Joplin data...'
     @joplin_notes = @notes.map do |note|
       {
         id: generate_id,
         title: note[:title],
         content: note[:markdown],
         turtl_id: note[:id],
+        file: note[:file],
         tags: store_tags(note[:tags]),
         parent_id: select_note_parent(note),
         type_: 1
@@ -138,7 +144,6 @@ class TurtlJoplinConverter
   end
 
   def convert_tags
-    puts 'Converting Tags to Joplin data...'
     @joplin_tags = @tags.flatten.uniq.map do |tag|
       {
         id: generate_id,
@@ -149,19 +154,6 @@ class TurtlJoplinConverter
     end
   end
 
-  def common_joplin_params
-    {
-      id: generate_id,
-      encryption_cipher_text: nil,
-      encryption_applied: 0,
-      is_shared: 0,
-      created_time: Time.now.utc.iso8601,
-      updated_time: Time.now.utc.iso8601,
-      user_created_time: Time.now.utc.iso8601,
-      user_updated_time: Time.now.utc.iso8601
-    }
-  end
-
   def generate_id
     hex = SecureRandom.hex
     @ids.include?(hex) ? generate_id : @ids.push(hex) && hex
@@ -169,59 +161,55 @@ class TurtlJoplinConverter
 
   def write_joplin_raw_data
     FileUtils.mkdir_p('raw/resources')
-    write_spaces && write_boards && write_notes && write_tags && write_tag_joins
+    write_notebooks && write_image_notes && write_std_notes && write_tags && write_tag_joins
   end
 
-  def write_spaces
-    puts 'Exporting Spaces as Joplin markdown files...'
+  def write_notebooks
+    @joplin_spaces.each { |space| File.write("raw/#{space[:id]}.md", notebook_joplin_format(space).strip) }
+    @joplin_boards.each { |board| File.write("raw/#{board[:id]}.md", notebook_joplin_format(board).strip) }
+  end
 
-    @joplin_spaces.each do |space|
-      File.write("raw/#{space[:id]}.md", notebook_joplin_format(space).strip)
+  def write_image_notes
+    @joplin_notes.select { |note| note[:file] }.each do |note|
+      file_id = write_file(note)
+      note[:content] = image_tag(file_id, note[:file]) + note[:content]
+      File.write("raw/#{note[:id]}.md", note_joplin_format(note).strip)
     end
   end
 
-  def write_boards
-    puts 'Exporting Boards as Joplin markdown files...'
+  def image_tag(file_id, file)
+    <<~IMAGE
+      <img src=\":/#{file_id}\" alt=\"#{file['name']}\" width=\"#{file['meta']['width']}\" height=\"#{file['meta']['height']}\">
 
-    @joplin_boards.each do |board|
-      File.write("raw/#{board[:id]}.md", notebook_joplin_format(board).strip)
-    end
+    IMAGE
   end
 
-  def write_notes
-    puts 'Exporting Notes as Joplin markdown files...'
+  def write_file(note)
+    file_id = generate_id
+    file_ext = File.extname(note.dig(:file, 'name')).delete('.')
+    File.write("raw/#{file_id}.md", file_joplin_format(note, file_id, file_ext).strip)
+    File.write("raw/resources/#{file_id}.#{file_ext}", Base64.decode64(note.dig(:file, :data).first[:data]))
+    file_id
+  end
 
-    @joplin_notes.each do |note|
+  def write_std_notes
+    @joplin_notes.select { |note| note[:file].nil? }.each do |note|
       File.write("raw/#{note[:id]}.md", note_joplin_format(note).strip)
     end
   end
 
   def write_tags
-    puts 'Exporting Tags as Joplin markdown files...'
-
-    @joplin_tags.each do |tag|
-      File.write("raw/#{tag[:id]}.md", tag_joplin_format(tag).strip)
-    end
+    @joplin_tags.each { |tag| File.write("raw/#{tag[:id]}.md", tag_joplin_format(tag).strip) }
   end
 
   def write_tag_joins
     @joplin_notes.select { |note| note[:tags].any? }.each do |note|
-      # note[:tags] = ["foo", "bar"]
       note[:tags].each do |tag_title|
         tag_id = @joplin_tags.select { |joplin_tag| joplin_tag[:title] == tag_title }.first[:id]
         tag_join_id = generate_id
         File.write("raw/#{tag_join_id}.md", tag_join_joplin_format(tag_join_id, tag_id, note[:id]).strip)
       end
     end
-  end
-
-  def tag_join_joplin_format(tag_join_id, tag_id, note_id)
-    <<~MD
-      id: #{tag_join_id}
-      note_id: #{note_id}
-      tag_id: #{tag_id}
-      #{timestamps_md}#{metadata_md}type_: 6
-    MD
   end
 
   def notebook_joplin_format(notebook)
@@ -246,6 +234,19 @@ class TurtlJoplinConverter
     MD
   end
 
+  def file_joplin_format(note, file_id, file_ext)
+    <<~MD
+      #{note.dig(:file, 'name')}
+
+      id: #{file_id}
+      mime: #{note.dig(:file, 'type')}
+      filename:
+      file_extension: #{file_ext}
+      size: #{note.dig(:file, 'size')}
+      #{timestamps_md}#{metadata_md}type_: 4
+    MD
+  end
+
   def tag_joplin_format(tag)
     <<~MD
       #{tag[:title]}
@@ -256,7 +257,14 @@ class TurtlJoplinConverter
     MD
   end
 
-
+  def tag_join_joplin_format(tag_join_id, tag_id, note_id)
+    <<~MD
+      id: #{tag_join_id}
+      note_id: #{note_id}
+      tag_id: #{tag_id}
+      #{timestamps_md}#{metadata_md}type_: 6
+    MD
+  end
 
   def timestamps_md
     <<~TIME
@@ -293,11 +301,6 @@ class TurtlJoplinConverter
       markup_language: 1
     NOTES
   end
-end
-
-unless ARGV.length == 1
-  puts 'Pass the file as an argument'
-  exit
 end
 
 TurtlJoplinConverter.new(ARGV[0]).call
